e763727ae1189a6743af201328331aecee6385c9
Siddharth Seth <sidat289@gmail.com>
2018-11-21 19:17:07 +0530

    detour 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import profileImage from './profile.jpg'
-import image from './image.png'
-
-let imageStyle = {
-  backgroundImage: 'url(' + image + ')'
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="left-container">
+        <div className="col-5 left-container">
           <div>
             <h1>
               Hello
@@ -25,18 +19,19 @@ class App extends Component {
           </div>
           <div className="list-inline">
             <p>Reach me at <a rel="noopener noreferrer" target="_blank" href="mailto:mail@siddharthseth.xyz">mail@siddharthseth.xyz</a></p>
+            <h5>or through</h5>
             <div>
-              <a href="#" className="fab fa-github"></a>
-              <a href="#" className="fab fa-instagram"></a>
-              <a href="#" className="fab fa-linkedin"></a>
+              <a href="#" className="media-buttons fab fa-github"></a>
+              <a href="#" className="media-buttons fab fa-instagram"></a>
+              <a href="#" className="media-buttons fab fa-linkedin"></a>
             </div>
           </div>
         </div>
-        <div className="right-container">
+        <div className="col-7 right-container">
           <div className="nav-bar">
-            <div className="flexbox-left">
+            {/* <div className="flexbox-left">
               <h4>SIDDHARTH SETH</h4>
-            </div>
+            </div> */}
             <div className="flexbox-right">
               <button href="#projects">PROJECTS</button>
               <button href="#resume">RÉSUMÉ</button>
@@ -45,12 +40,12 @@ class App extends Component {
           <div className="content">
             <h3>About Me</h3>
             <p>
-              I am a software developer from Lucknow, India. I have worked on and published several Android Apps, which are available on the Google Play Store and on my Github page(chrome289).
+              I am a software developer from Lucknow, India. I have worked on and published several Android Apps, which are available on the Google Play Store and on my <a href="https://www.github.com/chrome289">Github page</a>.
           </p>
             <p>
               My Android projects are done using Kotlin or Java and Node.js is my choice of Server Runtime-Environment. Besides these I have experience in working on Windows' WPF and Console applications using C# and Java. Most of my projects are Open-Source and the source codes are available on Github.
           </p>
-            <h5>Outside the Tech-World</h5>
+            <h5>Outside the Tech World</h5>
             <p>
               I like going on long walks and being surrounded by nature. I am a self-confessed Europhile and have an intermediate knowledge of French(Peut-être?). I also follow F1 Racing and read Fiction Books in my past-time.
           </p>
