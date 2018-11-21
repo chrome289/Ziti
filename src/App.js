9a068c81bfca89e89090265b7d01d808affa621f
Siddharth Seth <sidat289@gmail.com>
2018-11-21 23:50:48 +0530

    frontpage functional

--- src/App.js
+++ src/App.js
@@ -18,29 +18,20 @@ class App extends Component {
             </h4>
           </div>
           <div className="list-inline">
-            <p>Reach me at <a rel="noopener noreferrer" target="_blank" href="mailto:mail@siddharthseth.xyz">mail@siddharthseth.xyz</a></p>
+            <p>Reach me at <a target="_blank" rel="noopener noreferrer" href="mailto:mail@siddharthseth.xyz">mail@siddharthseth.xyz</a></p>
             <h5>or through</h5>
             <div>
-              <a href="#" className="media-buttons fab fa-github"></a>
-              <a href="#" className="media-buttons fab fa-instagram"></a>
+              <a target="_blank" href="https://www.github.com/chrome289" className="media-buttons fab fa-github"></a>
+              <a target="_blank" href="https://www.instagram.com/siddharthseth289" className="media-buttons fab fa-instagram"></a>
               <a href="#" className="media-buttons fab fa-linkedin"></a>
             </div>
           </div>
         </div>
         <div className="col-7 right-container">
-          <div className="nav-bar">
-            {/* <div className="flexbox-left">
-              <h4>SIDDHARTH SETH</h4>
-            </div> */}
-            <div className="flexbox-right">
-              <button href="#projects">PROJECTS</button>
-              <button href="#resume">RÉSUMÉ</button>
-            </div>
-          </div>
           <div className="content">
             <h3>About Me</h3>
             <p>
-              I am a software developer from Lucknow, India. I have worked on and published several Android Apps, which are available on the Google Play Store and on my <a href="https://www.github.com/chrome289">Github page</a>.
+              I am a software developer from Lucknow, India. I have worked on and published several Android Apps, which are available on the <a target="_blank" href="https://play.google.com/store/apps/developer?id=Siddharth+Seth">Google Play Store</a> and on my <a target="_blank" href="https://www.github.com/chrome289">Github page</a>.
           </p>
             <p>
               My Android projects are done using Kotlin or Java and Node.js is my choice of Server Runtime-Environment. Besides these I have experience in working on Windows' WPF and Console applications using C# and Java. Most of my projects are Open-Source and the source codes are available on Github.
@@ -50,8 +41,17 @@ class App extends Component {
               I like going on long walks and being surrounded by nature. I am a self-confessed Europhile and have an intermediate knowledge of French(Peut-être?). I also follow F1 Racing and read Fiction Books in my past-time.
           </p>
             <p>
-              Do check out my Projects and Résumé above and get in touch if you find it interesting.
+              Do check out my Projects and Résumé below and get in touch if you find it interesting.
           </p>
+          </div>
+          <div className="nav-bar">
+            {/* <div className="flexbox-left">
+              <h4>SIDDHARTH SETH</h4>
+            </div> */}
+            <div className="flexbox-right">
+              <button href="#projects">PROJECTS</button>
+              <button href="#resume">RÉSUMÉ</button>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Projects extends Component{
+    render(){
+        return(
+            <div className="projects">
+
+            </div>
+        );
+    }
+}
